3634550bf7cb8479c2d4fde7759e9671d856b501
Michał Kruk <michal.k.kruk@gmail.com>
2020-06-09 19:14:27 +0200

    add basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <h1 class="selected">
+      DAILY GIFS
+    </h1>
     <TrendingGifs />
   </div>
 </template>
@@ -16,12 +19,31 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background-color: #171c26;
+}
+
+.selected {
+  position: relative;
+  color: #ffce47;
+  font-size: 100px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -5px;
+    left: 40%;
+    width: 20%;
+    height: 10px;
+    background-color: #03c0b6;
+    border-radius: 50px;
+  }
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  @import url("https://fonts.googleapis.com/css2?family=Barlow:wght@900&display=swap");
+  font-family: "Barlow", sans-serif;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/TrendingGifs.vue
+++ src/components/TrendingGifs.vue
@@ -2,7 +2,10 @@
   <div class="trending">
     <div class="container">
       <div v-for="(gif, index) in gifs" :key="index" class="single-gif">
-        <img :src="gif.images.downsized.url" alt="" />
+        <img :src="gif.images.downsized.url" alt="" class="gif-small" />
+        <div class="copy">
+          <img src="@/assets/copy-icon.svg" alt="copy-icon" class="copy-icon" />
+        </div>
       </div>
     </div>
   </div>
@@ -26,3 +29,62 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.trending {
+  .container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+  }
+
+  .single-gif {
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    flex: 1 1 33%;
+    max-width: 400px;
+    min-height: 400px;
+    background-color: #6c7689;
+    margin: 10px;
+    border-radius: 30px;
+    transition: all 0.3s;
+
+    .gif-small {
+      max-width: 370px;
+      max-height: 370px;
+      border-radius: 25px;
+    }
+
+    &:hover {
+      background-color: #ffce47;
+    }
+
+    .copy {
+      display: flex;
+      position: absolute;
+      justify-content: center;
+      align-items: center;
+      bottom: 20px;
+      right: 20px;
+      height: 50px;
+      width: 50px;
+      background-color: #171c26;
+      border-radius: 50px;
+      opacity: 0.1;
+      transition: all 0.3s;
+      cursor: pointer;
+
+      .copy-icon {
+        height: 30px;
+        width: 30px;
+      }
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+}
+</style>
