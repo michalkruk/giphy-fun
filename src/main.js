f6d22b284d492ca9565a0e224642d1832d369bee
Michał Kruk <michal.k.kruk@gmail.com>
2020-06-12 00:31:31 +0200

    add loader, refactor navigation, add lib to notifications

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
+import Notifications from "vue-notification";
 
 Vue.prototype.$http = axios;
+Vue.use(Notifications);
 
 Vue.config.productionTip = false;
 
